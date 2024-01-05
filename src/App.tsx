a0f36cccacf026614680e6c4207e1ab592c3aaab
Duncanwar <semunda9@gmail.com>
2024-01-05 17:36:23 +0200

    exercise of a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
+import Alert from "./components/Alert"
 import { Button } from "./components/Button"
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false)
   return (
-   <Button color="danger" onClick={()=>console.log('clicked')}>My Button</Button>
+    <>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>My Button</Button>
+    </>
+
   )
 }
 
