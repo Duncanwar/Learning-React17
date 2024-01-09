6e9dfe57ba6d6be388528b49c8a2de4bb6b4d308
Duncanwar <semunda9@gmail.com>
2024-01-09 12:15:27 +0200

    vanilla css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 import Alert from "./components/Alert"
 import { Button } from "./components/Button"
+import ListGroup from "./components/ListGroup/ListGroup"
 
 function App() {
+  const items = ["New York","Kigali","Bujumbura"]
   const [alertVisible, setAlertVisibility] = useState(false)
   return (
     <>
-      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
-      <Button color="danger" onClick={() => setAlertVisibility(true)}>My Button</Button>
+    <ListGroup heading="Miami" items={items} onSelectItem={()=>{}} />
     </>
 
   )
