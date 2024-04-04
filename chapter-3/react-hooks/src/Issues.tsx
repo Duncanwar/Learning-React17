02d3d9f223592c293733918fc82030c4fda9616a
Duncanwar <semunda9@gmail.com>
2024-04-04 16:53:53 +0200

    functional componet

--- chapter-3/react-hooks/src/Issues.tsx
+++ chapter-3/react-hooks/src/Issues.tsx
@@ -1,5 +1,5 @@
 // Dependencies
-import { Component } from "react";
+import { FC, useState, useEffect } from "react";
 import axios from "axios";
 
 // Types
@@ -9,27 +9,18 @@ type Issue={
     state:string
 }
 
-type Props = {}
-type State = {issues:Issue[]};
 
-class Issues extends Component<Props, State>{
-    constructor(props:Props){
-        super(props)
-        this.state = {
-            issues:[]
-        }
-    }
 
-    componentDidMount() {
+const Issues : FC=()=>{
+    const [issues, setIssues] = useState<Issue[]>([]);
+
+    useEffect(()=> {
         axios.get('https://api.github.com/repos/ContentPI/ContentPI/issues')
         .then((response:any)=>{
-            this.setState({issues:response.data
-            })
+            setIssues(response.data)
         })
-    }
+    },[])
 
-    render(){
-        const {issues=[]} = this.state
         return(
             <>
             <h1>ContentPI Issues</h1>
@@ -42,7 +33,6 @@ class Issues extends Component<Props, State>{
             ))}
             </>
         )
-    }
 }
 
 export default Issues
